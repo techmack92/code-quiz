984029419a089dbd8e4e0a717072b05d1755fa62
Mack <mldixon9750@gmail.com>
2023-04-12 12:41:03 -0700

    added separate divs for headers in HTML; added variables, array of questions/answers and event listener for start button in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,72 @@
-var timerEl = document.getElementById('countdown');
+var countdown = document.getElementById("countdown");
+var startBtn = document.getElementById("start-quiz");
+var startScreen = document.getElementById("start-screen")
+
+// var timeLeft = document.getElementById("")
+
+// Removes the start screen and button, but keeps the "View High Scores" and Timer
+startBtn.addEventListener("click", function() {
+    startScreen.remove();
+});
+  
+// List of all questions, choices, and answers
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts",
+    },
+
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses",
+    },
+    
+    {
+      question: "Arrays in JavaScript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above",
+    },
+
+    {
+      question: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes",
+    },
+    
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log",
+    },
+  ];
   
   
+//   // Timer that counts down from 75
+// function countdown() {
+//     var timeLeft = 75;
   
   
-  
-  // Timer that counts down from 75
-function countdown() {
-    var timeLeft = 75;
-  
-  
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
-    }
-  }, 1000);
-}
+//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//   var timeInterval = setInterval(function () {
+//     // As long as the `timeLeft` is greater than 1
+//     if (timeLeft > 1) {
+//       // Set the `textContent` of `timerEl` to show the remaining seconds
+//       timerEl.textContent = timeLeft + " seconds remaining";
+//       // Decrement `timeLeft` by 1
+//       timeLeft--;
+//     } else if (timeLeft === 1) {
+//       // When `timeLeft` is equal to 1, rename to "second" instead of "seconds"
+//       timerEl.textContent = timeLeft + " second remaining";
+//       timeLeft--;
+//     } else {
+//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+//       timerEl.textContent = ";
+//       // Use `clearInterval()` to stop the timer
+//       clearInterval(timeInterval);
+//       // Call the `displayMessage()` function
+//       displayMessage();
+//     }
+//   }, 1000);
+// }
