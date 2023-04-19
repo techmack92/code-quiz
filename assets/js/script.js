9e4d6b3b5582e79ab758cf404fe33c443881a742
Mack <mldixon9750@gmail.com>
2023-04-18 18:40:40 -0700

    fixed functions in JS so the checkAnswer logic and endQuiz logic works

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ var scoreEl = document.getElementById("score");
 var submitBtnEl = document.getElementById("submit-btn");
 var questionTitleEl = document.getElementById("question-title");
 var currentQuestionIndex = 0;
+var currentQuestion;
 var score = 0;
 var timerInterval = 0;
 var timeLeft = 75;              // Timer that counts down from 75
@@ -67,61 +68,49 @@ function countdown() {
 
 // Function that displays questions
 function showQuestion() {
+
     quizEl.style.display = "block";
     choicesEl.style.display = "block";
-    var currentQuestion = questions[currentQuestionIndex];       // Gets current question from `questions` array using the index
+    currentQuestion = questions[currentQuestionIndex];       // Gets current question from `questions` array using the index
+  
     questionTitleEl.textContent = currentQuestion.question;      // Sets text of `quiz` element to the current question's text
     choicesEl.innerHTML = "";
     for (var i = 0; i < currentQuestion.choices.length; i++) {   // As long as `i` is less than the length of `questions` array,
         var li = document.createElement("li");                   // Create a list item element, and
         li.textContent = currentQuestion.choices[i];             // Set the choices text of the current question
         li.classList.add("button");
-        li.addEventListener("click", function () {
-            if (this.textContent === currentQuestion.answer) {
-                score++;
-            } else {
-                timeLeft -=10;
-            }
-            currentQuestionIndex++;
-            if (currentQuestionIndex === questions.length || timeLeft === 0) {
-                endQuiz();
-            } else {
-                showQuestion();
-            }
-        }); 
+        li.addEventListener("click", checkAnswer) 
         choicesEl.appendChild(li);                                // Appends `li` elements to the `choicesEl` div
     }
 }
 
+// Function that checks the user's answer
+function checkAnswer(event) {
+    // event.preventDefault();
+    var selectedChoice = event.target.textContent;               // Gets the text from user's selected choice
+    if (selectedChoice == currentQuestion.answer) {             // If user chooses correct answer,
+      score++;                                                   // Score increases by 1
+      checkAnswerEl.textContent = "Correct!";                    // Display "Correct!"
+    } else {
+      timeLeft -= 10;                                            // Otherwise, user loses 10 seconds off timer
+      checkAnswerEl.textContent = "Wrong! The correct answer is: " + currentQuestion.answer;       // Tells user they chose wrong answer & displays correct answer
+    }
+    currentQuestionIndex++;                                          // Goes to next question
+    checkQuestionIndex(currentQuestionIndex);
+}
   
+function checkQuestionIndex(currentQuestionIndex) {
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {  
+    endQuiz();
+  }
+}
 
-// // Function that checks the user's answer
-// function checkAnswer(event) {
-//     event.preventDefault();
-//     var selectedChoice = event.target.textContent;          // Gets the text from user's selected choice
-//     var currentQuestion = questions[currentQuestionIndex];
-  
-//     if (selectedChoice === currentQuestion.answer) {        // If user chooses correct answer,
-//       score++;                                              // Score increases by 1
-//       checkAnswerEl.textContent = "Correct!";               // Display "Correct!"         
-//     } else {
-//       timeLeft -= 10;                                       // Otherwise, user loses 10 seconds off timer
-//       checkAnswerEl.textContent = "Wrong! The correct answer is: " + currentQuestion.answer;       // Tells user they chose wrong answer & displays correct answer
-//     }
-// }
-// currentQuestionIndex++;                                     // Goes to next question
-  
-// if (currentQuestionIndex === questions.length) {
-//   endQuiz();
-// } else {        
-//   quizEl.removeChild(quizEl.lastChild);                     // Removes previous question and choices
-//   showQuestion();                                           // Shows next question
-// }
-
-//   // Funciton that displays final score at end of quiz
-//   function endQuiz() {
-//     clearInterval(timerInterval);
-//     timerEl.textContent = "Time's up!";
-//     finalScoreEl.textContent = score;                       // Shows final score
-//     submitBtnEl.removeEventListener("click", submitScore);  // Removes submit button to prevent user from submitting initials multiple times
-//   }
+// Funciton that displays final score at end of quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  timerEl.textContent = "Time's up!";
+  finalScoreEl.textContent = score;                       // Shows final score
+  // submitBtnEl.removeEventListener("click", submitScore);  // Removes submit button to prevent user from submitting initials multiple times
+}
